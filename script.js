4b1e974b511a52d258c16dda2f1cf671c3dad7b0
Palak <vidhijain626@gmail.com>
2020-09-20 09:26:41 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const audioElement =document.getElementById('audio');
+const button=document.getElementById('button');
+const robotVoice = new Audio('robot.mp3');
+function convertToSpeech(joke){
+    VoiceRSS.speech({
+        key: 'fab0e4a1283f4fc986876be0fa2b1d14',
+        src: joke,
+        hl: 'en-in',
+        v: 'Jai',
+        r: -1, 
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+
+}
+
+// Get Jokes from the API
+
+async function getJoke(){
+    robotVoice.play();
+    let joke='';
+    const apiUrl='https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes/1';
+    try{
+        const response=await fetch(apiUrl);
+        const data=await response.json();
+        const [jokeText]=data;
+        const {setup,punchline}=jokeText;
+        joke=`${setup} ...${punchline}`;
+        console.log(joke);
+       convertToSpeech(joke);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+// Event Listeners
+button.addEventListener('click',getJoke);
+audioElement.addEventListener('play',()=>{
+    robotVoice.pause();
+    button.disabled=true;
+})
+audioElement.addEventListener('ended',()=>{
+    button.disabled=false;
+})
